1ba4b4689c2fd169334ee1133052ff77a5cb8bc3
minju <minju9711@naver.com>
2024-11-19 02:24:33 +0900

    설정: AWS S3 관련 설정 및 통합 - s3 디렉토리 추가

---
+++ src/main/java/SwProject/config/s3/S3Config.java
@@ -0,0 +1,37 @@
+package SwProject.config.s3;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Data
+public class S3Config {
+
+    @Value("${cloud.aws.credentials.accesskey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretkey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.s3.bucketName}")
+    private String bucketName;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .build();
+    }
+
+}
