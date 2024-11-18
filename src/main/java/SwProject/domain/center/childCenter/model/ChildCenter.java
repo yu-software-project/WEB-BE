820659960ee274c6499d2c6463c6da472f045204
minju <minju9711@naver.com>
2024-11-19 02:32:24 +0900

    자녀 센터 데이터 모델: 자녀 센터의 데이터 구조와 속성을 정의하는 모델 클래스 구현

---
+++ src/main/java/SwProject/domain/center/childCenter/model/ChildCenter.java
@@ -0,0 +1,53 @@
+package SwProject.domain.center.childCenter.model;
+
+import SwProject.domain.facility.facilityIntroduction.model.FacilityIntroduction;
+import SwProject.domain.greetings.domain.Greetings;
+import SwProject.domain.routeInfo.domain.RouteInfo;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Table(name = "child_center")
+public class ChildCenter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "child_center_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String phoneNumId;
+
+    @Column(nullable = false, length = 255)
+    private String ceoName;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String centerName;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String roadAddress;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String detailAddress;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String certificate;
+
+    @OneToOne(cascade = CascadeType.REMOVE) //해당 db삭제시, 연결된 db 모두 삭제됨
+    @JoinColumn(name = "facility_introduction_id")
+    private FacilityIntroduction facilityIntroduction;
+
+    @OneToOne(cascade = CascadeType.REMOVE) //해당 db삭제시, 연결된 db 모두 삭제됨
+    @JoinColumn(name = "greetings_id")
+    private Greetings greetings;
+
+    @OneToOne(cascade = CascadeType.REMOVE) //해당 db삭제시, 연결된 db 모두 삭제됨
+    @JoinColumn(name = "route_info_id")
+    private RouteInfo routeInfo;
+
+
+}
