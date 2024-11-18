101b0b95c7a94171de858793cec9a96fe975f7a8
minju <minju9711@naver.com>
2024-11-19 02:17:12 +0900

    실행 파일 추가: ChildrenHeavenMakersApplication.java

---
+++ src/main/java/SwProject/ChildrenHeavenMakersApplication.java
@@ -0,0 +1,13 @@
+package SwProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildrenHeavenMakersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildrenHeavenMakersApplication.class, args);
+	}
+
+}
