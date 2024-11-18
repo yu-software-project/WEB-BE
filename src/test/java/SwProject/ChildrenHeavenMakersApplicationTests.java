e956cfd36051dbf1ce72f9a5336b6fb63e5b4362
minju <minju9711@naver.com>
2024-11-19 02:10:43 +0900

    테스트 코드 추가: src/test/ 디렉토리 초기 커밋

---
+++ src/test/java/SwProject/ChildrenHeavenMakersApplicationTests.java
@@ -0,0 +1,13 @@
+package SwProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildrenHeavenMakersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
