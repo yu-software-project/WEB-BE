1304e71b1caa47e68a5ec130ad03d47a7490ecfd
minju <minju9711@naver.com>
2024-11-19 02:24:34 +0900

    설정: Swagger API 문서화 설정 - swagger 디렉토리 추가

---
+++ src/main/java/SwProject/config/swagger/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package SwProject.config.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "아지트 API 명세서",
+                description = "아지트 서비스 API 명세서",
+                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+    private static final String BEARER_TOKEN_PREFIX="Bearer";
+    @Bean
+    public OpenAPI openAPI() {
+        String securityJwtName = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
+        Components components = new Components()
+                .addSecuritySchemes(securityJwtName, new SecurityScheme()
+                        .name(securityJwtName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme(BEARER_TOKEN_PREFIX)
+                        .bearerFormat(securityJwtName));
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
